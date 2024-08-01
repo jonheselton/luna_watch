83347dbc0e27f3e59729938a0f7a7ae05d1bc4aa
jonheselton <git@github.com>
2024-08-01 09:13:24 -0500

    models, routes, and views

--- app_luna_watch/urls.py
+++ app_luna_watch/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
 from . import views
+
+app_name = 'app_luna_watch'
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path('', views.index, name='index'),
+    path('pets/', views.pets, name = 'pets'),
+    path('<pet>/pet_details', views.pet_details, name = 'pet_details')
 ]
